e2c55896ac76fdbf380527b8858551eeb2af908f
Duy Anh <ngoduyanh1607@gmail.com>
2021-01-15 03:09:15 +0700

    basic knuth

--- src/Rmit/Guess.java
+++ src/Rmit/Guess.java
@@ -2,16 +2,68 @@ package Rmit;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class Guess {
-    int initial_guess = 1122;
-    public static int indexOfSmallest(List<Integer>array ){
+    public static int initial_guess = 1123;
+    public static List<Integer> arrlist = new ArrayList<>()  ;
+    public static GuessRunner guessRunner = new GuessRunner();
+    public static List<Result> results = new ArrayList<>();
+    public static Result result = new Result();
+    public static Iterator<Integer> possibleTemp;
+    public static int currentNum;
+    public static int make_guess(int hits, int strike) {
+//        System.out.println(hits);
+//        System.out.println(strike);
+//        System.out.println(arrlist);
+        if (arrlist.size() == 0 && hits ==0 && strike ==0){
+//            System.out.println("here1");
+            possibleGuess();
+            resultsList();
+            possibleTemp = arrlist.iterator();
+            currentNum = initial_guess;
+            return initial_guess;
+        }
+        else {
+
+            while (possibleTemp.hasNext()) {
+                int num = possibleTemp.next();
+                Result temp = guessRunner.processGuess(num, currentNum);
+                if (temp.getHits() != hits || temp.getStrikes() != strike) {
+                    possibleTemp.remove();
+                }
+            }
+            possibleTemp = arrlist.iterator();
+            int next_num = arrlist.get(get_next_num(arrlist, results));
+            currentNum = next_num;
+            return next_num;
+        }
+    }
+
+    public static List<Integer> possibleGuess() {
+        for (int i = 1000; i < 10000; i++) {
+            arrlist.add(i);
+        }
+//        Iterator<Integer> possibleTemp = arrlist.iterator();
+        return arrlist;
+    }
+
+    public static List<Result> resultsList() {
+        for (int i= 0; i <5; i++){
+            for(int j=0;j < 5-i ;j++){
+                results.add(new Result(i,j));
+            }
+        }
+        return results;
+    }
+
+    public static int indexOfSmallest(List<Integer> array) {
         int index = 0;
         int min = array.get(index);
 
-        for (int i = 1; i < array.size(); i++){
-            if (array.get(i) < min){
+        for (int i = 1; i < array.size(); i++) {
+            if (array.get(i) < min) {
                 min = array.get(i);
                 index = i;
             }
@@ -19,11 +71,11 @@ public class Guess {
         return index;
     }
 
-    public static int maxInList(List<Integer>array){
+    public static int maxInList(List<Integer> array) {
         int max = array.get(0);
         int index = 0;
-        for (int i = 1; i < array.size(); i++){
-            if (array.get(i) > max){
+        for (int i = 1; i < array.size(); i++) {
+            if (array.get(i) > max) {
                 max = array.get(i);
                 index = i;
             }
@@ -31,12 +83,12 @@ public class Guess {
         return max;
     }
 
-    public static int getRemainCount(List<Integer> arr,int target, Result result){
-        int count =0;
+    public static int getRemainCount(List<Integer> arr, int target, Result result) {
+        int count = 0;
         GuessRunner guessRunner = new GuessRunner();
-        for (int i =0; i < arr.size();i++){
-            Result temp = guessRunner.processGuess(target,arr.get(i));
-            if (temp.getHits() == result.getHits() && temp.getStrikes() == result.getStrikes()){
+        for (int i = 0; i < arr.size(); i++) {
+            Result temp = guessRunner.processGuess(target, arr.get(i));
+            if (temp.getHits() == result.getHits() && temp.getStrikes() == result.getStrikes()) {
                 count++;
             }
         }
@@ -44,22 +96,24 @@ public class Guess {
         return count;
     }
 
-    public static int getMaxDifferentCount(List<Integer>arr, List<Result> results,int target){
+    public static int getMaxDifferentCount(List<Integer> arr, List<Result> results, int target) {
         List<Integer> temp_diff = new ArrayList<>();
-        for (int i =0; i < results.size();i++){
-            int count = getRemainCount(arr,target,results.get(i));
+        for (int i = 0; i < results.size(); i++) {
+            int count = getRemainCount(arr, target, results.get(i));
             temp_diff.add(count);
         }
 //        System.out.println("this is temp diff size " +temp_diff.size());
         return maxInList(temp_diff);
     }
-    public static int get_next_num(List<Integer> arr, List<Result> results){
+
+    public static int get_next_num(List<Integer> arr, List<Result> results) {
         List<Integer> temp_int = new ArrayList<Integer>();
-        for (int i =0; i < arr.size();i++){
-            temp_int.add(getMaxDifferentCount(arr,results,arr.get(i)));
+        for (int i = 0; i < arr.size(); i++) {
+            temp_int.add(getMaxDifferentCount(arr, results, arr.get(i)));
         }
-//        System.out.println("this is array size"+ arr.size());
-//        System.out.println(temp_int.size());
+
         return indexOfSmallest(temp_int);
     }
+
+
 }
--- src/Rmit/GuessRunner.java
+++ src/Rmit/GuessRunner.java
@@ -1,15 +1,20 @@
 package Rmit;
 
-public class GuessRunner {
+import javax.swing.*;
+//import java.util.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
+public class GuessRunner {
     static Result processGuess(int target, int guess) {
         char des[] = Integer.toString(target).toCharArray();
         char src[] = Integer.toString(guess).toCharArray();
-        int hits=0;
-        int strikes=0;
+        int hits = 0;
+        int strikes = 0;
 
         // process strikes
-        for (int i=0; i<4; i++) {
+        for (int i = 0; i < 4; i++) {
             if (src[i] == des[i]) {
                 strikes++;
                 des[i] = 'a';
@@ -17,10 +22,10 @@ public class GuessRunner {
             }
         }
         // process hits
-        for (int i=0; i<4; i++) {
-            for (int j=0; j<4; j++) {
-                if (src[i]!='a') {
-                    if (src[i]==des[j]) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (src[i] != 'a') {
+                    if (src[i] == des[j]) {
                         hits++;
                         des[j] = 'a';
                         break;
@@ -29,20 +34,65 @@ public class GuessRunner {
             }
         }
 //        System.out.printf("\t");
-//        if (strikes==4)	{ // game over
+//        if (strikes == 4) { // game over
 //            System.out.printf("4 strikes - Game over\n");
 //            return new Result(hits, strikes);
 //        }
-
-//        if (hits==0 && strikes==0)
+//        if (hits == 0 && strikes == 0)
 //            System.out.printf("Miss\n");
-//        else if(hits>0 && strikes==0)
+//        else if (hits > 0 && strikes == 0)
 //            System.out.printf("%d hits\n", hits);
-//        else if(hits==0 && strikes>0)
+//        else if (hits == 0 && strikes > 0)
 //            System.out.printf("%d strikes\n", strikes);
-//        else if(hits>0 && strikes>0)
+//        else if (hits > 0 && strikes > 0)
 //            System.out.printf("%d strikes and %d hits\n", strikes, hits);
 
         return new Result(hits, strikes);
     }
+
+    public static void main(String[] args) {
+        // write your code here
+
+        FileWriter fileWriter;
+        try {
+            fileWriter = new FileWriter("result_knuth.csv",true);
+            fileWriter.append("target,first guess,number of guess,guesses \n");
+            fileWriter.flush();
+            fileWriter.close();
+            for (int i = 1000; i < 10000; i++) {
+                fileWriter = new FileWriter("result_knuth.csv",true);
+                int guess_cnt = 0;
+                int target = i;
+                Result res = new Result();
+                System.out.println("Guess\tResponse\n");
+                while (res.getStrikes() < 4) {
+                    int guess = Guess.make_guess(res.getHits(), res.getStrikes());
+                    if (guess_cnt == 0) {
+                        fileWriter.append(String.valueOf(target));
+                        fileWriter.append(",");
+                        fileWriter.append(String.valueOf(guess));
+                        fileWriter.append(",");
+                    }
+                    System.out.printf("%d\n", guess);
+                    if (guess == -1) {    // user quits
+                        System.out.printf("you quit: %d\n", target);
+                        return;
+                    }
+                    guess_cnt++;
+
+                    res = processGuess(target, guess);
+                }
+                Guess.arrlist = new ArrayList<>();
+                fileWriter.append(String.valueOf(guess_cnt));
+                fileWriter.append("\n");
+                System.out.printf("Target: %d - Number of guesses: %d\n", target, guess_cnt);
+                fileWriter.flush();
+                fileWriter.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e);
+        }
+    }
 }
+
